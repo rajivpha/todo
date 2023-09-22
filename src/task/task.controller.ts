666f53eb138436478c6d2c6b1ee3dc8d8c6c5bcb
rajiv.phanju <rajiv.phanju@infodevelopers.com.np>
2023-09-22 14:54:01 +0545

    [update] task work

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -7,18 +7,24 @@ import {
   Param,
   Delete,
   Render,
+  Res,
+  Put,
 } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Response } from 'express';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  async create(@Body() createTaskDto: CreateTaskDto) {
-    return await this.taskService.create(createTaskDto);
+  async create(@Res() res: Response, @Body() createTaskDto: CreateTaskDto) {
+    const task = await this.taskService.create(createTaskDto);
+    if (task) {
+      res.redirect('task');
+    }
   }
 
   @Get()
@@ -34,9 +40,17 @@ export class TaskController {
     return await this.taskService.findOne(+id);
   }
 
-  @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return await this.taskService.update(+id, updateTaskDto);
+  @Put(':id')
+  async update(
+    @Res() res: Response,
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
+    console.log(updateTaskDto);
+    const task = await this.taskService.update(+id, updateTaskDto);
+    if (task) {
+      res.redirect('task');
+    }
   }
 
   @Delete(':id')
